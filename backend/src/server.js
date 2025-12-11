4c300deb005b6726b5389f6e3d4fb65153538155
WebWorkingClub <webworkingclub@gmail.com>
2025-12-12 01:10:15 +0330

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,31 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inggest.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-  connectDB();
-});
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inggest", serve({ client: inngest, functions }));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server running on port ${ENV.PORT}`);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
